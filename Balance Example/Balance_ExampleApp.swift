54dd2d1a9107827c9fd9f21f6473321d92da118c
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-03-11 10:52:17 -0500

    Initial Commit

---
+++ Balance Example/Balance_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Balance_ExampleApp.swift
+//  Balance Example
+//
+//  Created by J. DeWeese on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Balance_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Balance Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Balance Example
+//
+//  Created by J. DeWeese on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
